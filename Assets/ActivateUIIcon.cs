2e59b353088dbfd2b41ac4df5967ff8173ff33a3
Lasse <lasse.a.linden@gmail.com>
2020-02-01 17:44:41 +0200

    New in game ui

---
+++ Assets/ActivateUIIcon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateUIIcon : MonoBehaviour
+{
+    private Image _image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Great piece of code
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ActivateUIICon();
+        }
+    }
+    void ActivateUIICon()
+    {
+        _image.color = new Color(255f, 255f, 255f, 1f);
+    }
+}
---
+++ Assets/DestroyAfter5Seconds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfter5Seconds : MonoBehaviour
+{
+    void Start()
+    {
+        //Fade Away the object instanstly
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        Destroy(gameObject);
+    }
+}
