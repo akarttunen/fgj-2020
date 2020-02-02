9d5c01dc7e49adfd8e9c0146bd8efe7967b04385
Aatu Karttunen <aatu.karttunen@gmail.com>
2020-02-02 02:34:38 +0200

    vihuja

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,9 +6,15 @@ public class Target : MonoBehaviour
 {
     public float health = 50f;
 
+    public GameObject collectable;
+    public AudioSource audioS;
+    public AudioClip damageSound;
+    public AudioClip deathSound;
+
     public void TakeDamage (float amount)
     {
         health -= amount;
+        audioS.PlayOneShot(damageSound);
         if(health <= 0f)
         {
             Die();
@@ -17,6 +23,24 @@ public class Target : MonoBehaviour
 
     void Die()
     {
+        if (CheckIfDrop())
+        {
+            Instantiate(collectable, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.identity);
+        }
+        audioS.PlayOneShot(deathSound);
         Destroy(gameObject);
     }
-}
+
+    bool CheckIfDrop()
+    {
+        Random.InitState((int)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds);
+        if (Random.Range(0, 100) % 1 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    }
