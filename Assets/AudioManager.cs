d3a14d153704607813dfc3a0f1fe6f5508f994eb
Lasse <lasse.a.linden@gmail.com>
2020-02-02 14:39:06 +0200

    Added amazing sound effects

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -7,10 +7,28 @@ public class AudioManager : MonoBehaviour
     public AudioSource intro;
     public AudioSource music;
     public AudioSource auto;
+    public AudioSource burpSound;
+    public AudioSource korkkaus;
+    public AudioSource perjantai;
     // Start is called before the first frame update
     void Start()
     {
         intro.Play();
         music.PlayDelayed(intro.clip.length);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            burpSound.PlayOneShot(burpSound.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            perjantai.PlayOneShot(perjantai.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            korkkaus.PlayOneShot(korkkaus.clip);
+        }
     }
 }
