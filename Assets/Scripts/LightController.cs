461abee283d838704b9876159185b3a97b59934f
Veeti Karttunen <veeti.karttunen@solteq.com>
2020-02-02 02:30:53 +0200

    Make flashlight

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Component flashlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = gameObject.GetComponent("Light");
+        //gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log(this.GetComponent<Light>().enabled);
+            gameObject.GetComponent<Light>().enabled = !gameObject.GetComponent<Light>().enabled;
+        }
+    }
+}
