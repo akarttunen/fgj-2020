b0f4a40bf973a7a8d7693f311f3e9c6bb31fedbb
Lasse <lasse.a.linden@gmail.com>
2020-02-02 04:22:40 +0200

    Finishing script finished

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int collectedItems;
+    public int currentHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectedItems = 0;
+        currentHp = 100;
+    }
+    void AddCollectedItemAmount()
+    {
+        collectedItems++;
+    }
+    void AdjustHpAmount(int amount)
+    {
+        currentHp += amount;
+    }
+}
